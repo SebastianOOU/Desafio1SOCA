05915580cc95db59ced819caee6e795baed727d7
Sebastian Osorio <sebastian.osorio15@udea.edu.co>
2024-09-18 17:17:49 -0500

    Se ha modificado la funcion (mostrar tipo onda)

--- Funciones/Funcion_print.cpp
+++ Funciones/Funcion_print.cpp
@@ -1,11 +1,21 @@
-void print_datos_onda(float operacion1, int operacion2/*, int operacion3*/){
+void print_datos_onda(float operacion1, int operacion2, int operacion3){
 
     lcd_1.clear();
     lcd_1.setCursor(3, 0);
     lcd_1.print("Tipo onda:");
-    lcd_1.setCursor(5, 1);
-    lcd_1.print(operacion1);
-    lcd_1.print("V");
+    lcd_1.setCursor(3, 1);
+
+    if (operacion3 == 1){
+        lcd_1.print("Triangular");
+    }else if(operacion3 == 2) {
+        lcd_1.setCursor(4, 1);
+        lcd_1.print("Senoidal");
+    }else if(operacion3 == 3){
+        lcd_1.print("Cuadrada");
+    }else{
+        lcd_1.setCursor(2, 1);
+        lcd_1.print("Desconocida");
+    }
     delay(2500);
 
     lcd_1.clear();
@@ -23,4 +33,6 @@ void print_datos_onda(float operacion1, int operacion2/*, int operacion3*/){
     lcd_1.print(operacion2);
     lcd_1.print(" Hz");
     delay(2500);
+
+    print_bienvenida();
 }
