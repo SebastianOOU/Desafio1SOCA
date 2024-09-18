2bf1f466ce944a42b9b38eff450853d85a614cfb
Sebastian Osorio <sebastian.osorio15@udea.edu.co>
2024-09-18 17:18:53 -0500

    Se modificaron algunas cosas (sigue con fallos)

--- Funciones/calcular_tipo_triangular_senoidal.cpp
+++ Funciones/calcular_tipo_triangular_senoidal.cpp
@@ -1,28 +1,28 @@
-int calcular_tipo_triangular_senoidal(int valores_ondas[]){
-  /*La función recibe valores globales (contador).
-    Recibe un arreglo dinamico.
-    Retorna valores (1,2 o 0) segun sea el caso.*/
+void calcular_tipo_triangular_senoidal(int valores_ondas[], int *ptr_dato){
+    /*La funcion recibe valores globales (contador).
+  Recibe un arreglo dinamico y un puntero.
+  */
     int cont1 = 0;
     int cont2 = 0;
+
     bool opcion1 =false, opcion2 = false, opcion3 = false;
 
     for (int i = 0; i < contador; i++){
 
         if (valores_ondas[i] >= 0 && valores_ondas[i - 1] < 0){
             if (opcion2){
-                opcion3 = true;//True para segunda cresta.
+                opcion3 = true;
                 opcion2 = false;
+
             }else{
-                opcion1 = true;//True para primer cresta.
+                opcion1 = true;
             }
         }
-
         if(opcion1 && valores_ondas[i] < 0){
             opcion1 = false;
             opcion2 = true;
         }
         if(opcion3 && valores_ondas[i] < 0){
-            opcion3 = false;
             break;
         }
         if (opcion1){
@@ -31,16 +31,7 @@ int calcular_tipo_triangular_senoidal(int valores_ondas[]){
         if (opcion3){
             cont2++;
         }
-
     }
-
-    int a = 0;
-    int b = cont1 - cont2;
-    if (cont1 == cont2){//Verifica si es Triangular.
-        a = 1;
-    }
-    if(b != 0){//Verifica si es Senoidal.
-        a = 2;
-    }
-    return a;
+    bool cuadrada = es_onda_cuadrada(valores_ondas, contador);
+    definir_tipo_onda(ptr_dato, cont1, cont2, cuadrada);
 }
